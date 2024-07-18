29f161d8c108a83ebfe063c88b9ba198f16e2f24
BrianLondon <brian@seatgeek.com>
2024-07-18 18:27:09 -0400

    cleanup lib level unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,132 +7,94 @@ extern crate fixed_derive;
 pub use fixed::{ReadFixed, WriteFixed};
 pub use parse::{Alignment, EncodingScheme, FixedDeserializable};
 
-// #[cfg(test)]
-// mod tests {
-//     use std::io::{self, Write};
+#[cfg(test)]
+mod tests {
+    use std::io::{Read, Write};
 
-//     use super::*;
+    use super::*;
 
-//     fn to_str(inp: Vec<u8>) -> String {
-//         use std::str;
-//         str::from_utf8(inp.as_slice()).unwrap().to_string()
-//     }
+    fn to_str(inp: Vec<u8>) -> String {
+        use std::str;
+        str::from_utf8(inp.as_slice()).unwrap().to_string()
+    }
 
-//     #[test]
-//     fn test_helper() {
-//         let buf: [u8; 13] =
-//             [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
+    #[test]
+    fn test_helper() {
+        let buf: [u8; 13] =
+            [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
 
-//         let mut v = Vec::new();
-//         let _ = v.write(&buf);
+        let mut v = Vec::new();
+        let _ = v.write(&buf);
 
-//         assert_eq!(to_str(v), "Hello, World!");
-//     }
+        assert_eq!(to_str(v), "Hello, World!");
+    }
 
-//     #[test]
-//     fn test_write_custom_basic() {
-//         struct Foo;
+    #[test]
+    fn test_write_custom_basic() {
+        struct Foo;
 
-//         impl WriteFixed for Foo {
-//             fn write_fixed(&self, buf: &mut dyn std::io::Write) -> Result<(), ()> {
-//                 buf.write("Foo".as_bytes()).map(|_| ()).map_err(|_| ())
-//             }
-//         }
+        impl WriteFixed for Foo {
+            fn write_fixed(&self, buf: &mut dyn std::io::Write) -> Result<(), ()> {
+                buf.write("Foo".as_bytes()).map(|_| ()).map_err(|_| ())
+            }
+        }
 
-//         let foo = Foo;
+        let foo = Foo;
 
-//         let mut v = Vec::new();
-//         let res = foo.write_fixed(&mut v);
+        let mut v = Vec::new();
+        let res = foo.write_fixed(&mut v);
 
-//         assert!(res.is_ok());
-//         assert_eq!(to_str(v), "Foo");
-//     }
+        assert!(res.is_ok());
+        assert_eq!(to_str(v), "Foo");
+    }
 
-//     // Name is left aligned, ten characters
-//     // value is right aligned three characters
-//     #[derive(Debug, PartialEq, Eq)]
-//     struct NumWord {
-//         name: String,
-//         value: u8,
-//     }
+    // Name is left aligned, ten characters
+    // value is right aligned three characters
+    #[derive(Debug, PartialEq, Eq)]
+    struct NumWord {
+        name: String,
+        value: u8,
+    }
 
-//     impl WriteFixed for NumWord {
-//         fn write_fixed(&self, buf: &mut dyn std::io::Write) -> Result<(), ()> {
-//             let _ = buf.write_fmt(format_args!("{:<10}{:>3}", self.name, self.value));
-//             Ok(())
-//         }
-//     }
+    impl WriteFixed for NumWord {
+        fn write_fixed(&self, buf: &mut dyn std::io::Write) -> Result<(), ()> {
+            let _ = buf.write_fmt(format_args!("{:<10}{:>3}", self.name, self.value));
+            Ok(())
+        }
+    }
 
-//     impl ReadFixed for NumWord {
-//         fn read_fixed<R: io::Read>(buf: &mut R) -> Result<Self, ()>
-//                 where Self: Sized {
-//             let mut s = String::new();
-//             let _ = buf.read_to_string(&mut s);
+    impl ReadFixed for NumWord {
+        fn read_fixed<R: Read>(buf: &mut R) -> Result<Self, ()>
+                where Self: Sized {
+            let mut s = String::new();
+            let _ = buf.read_to_string(&mut s);
 
-//             let name = s[0..10].trim_end().to_string();
-//             let num = s[10..].to_string();
-//             let value = num.trim_start().parse::<u8>().unwrap();
+            let name = s[0..10].trim_end().to_string();
+            let num = s[10..].to_string();
+            let value = num.trim_start().parse::<u8>().unwrap();
             
-//             Ok(NumWord { name, value })
-//         }
-//     }
+            Ok(NumWord { name, value })
+        }
+    }
 
-//     #[test]
-//     fn custom_struct_write() {
-//         let three = NumWord { name: "three".to_string(), value: 3 };
+    #[test]
+    fn custom_struct_write() {
+        let three = NumWord { name: "three".to_string(), value: 3 };
 
-//         let mut v = Vec::new();
-//         let res = three.write_fixed(&mut v);
+        let mut v = Vec::new();
+        let res = three.write_fixed(&mut v);
 
-//         assert!(res.is_ok());
-//         assert_eq!(to_str(v), "three       3");
-//     }
+        assert!(res.is_ok());
+        assert_eq!(to_str(v), "three       3");
+    }
 
-//     #[test]
-//     fn custom_struct_read() {
-//         let three = NumWord { name: "three".to_string(), value: 3 };
+    #[test]
+    fn custom_struct_read() {
+        let three = NumWord { name: "three".to_string(), value: 3 };
 
-//         let mut buf = "three       3".as_bytes();
-//         let decoded = NumWord::read_fixed(&mut buf).unwrap();
+        let mut buf = "three       3".as_bytes();
+        let decoded = NumWord::read_fixed(&mut buf).unwrap();
 
-//         assert_eq!(decoded, three);
-//     }
-
-//     // TODO: Delete me (this is not correct behavior)
-//     #[test]
-//     fn derive_dummy() {
-//         use fixed_derive::WriteFixed;
-        
-//         #[derive(WriteFixed)]
-//         struct Point {
-//             x: u64,
-//             y: u64,
-//         }
-
-//         let point = Point { x: 42, y: 3 };
-
-//         let mut v = Vec::new();
-//         let res = point.write_fixed(&mut v);
-
-//         assert!(res.is_ok());
-//         assert_eq!(to_str(v), "42        3         ");
-//     }
-
-//         // TODO: Delete me (this is not correct behavior)
-//         #[test]
-//         fn derive_dummy_read() {
-//             use fixed_derive::ReadFixed;
-
-//             #[derive(Debug, ReadFixed, Eq, PartialEq)]
-//             struct Point {
-//                 x: u64,
-//                 y: u64,
-//             }
-    
-//             // let point = Point { x: 42, y: 3 };
-
-//             let mut buf = "42        3         ".as_bytes();
-//             let point = Point::read_fixed(&mut buf).unwrap();
-//             assert_eq!(point, Point { x: 42, y: 3 });
-//         }
-// }
+        assert_eq!(decoded, three);
+    }
+}
--- tests/test_derive_read.rs
+++ tests/test_derive_read.rs
@@ -3,6 +3,7 @@ extern crate fixed_derive;
 
 use fixed_derive::ReadFixed;
 
+
 #[derive(Debug, ReadFixed, Eq, PartialEq)]
 struct Point {
     x: u64,
@@ -17,3 +18,4 @@ fn derive_read_struct() {
     let point = Point::read_fixed(&mut buf).unwrap();
     assert_eq!(point, Point { x: 42, y: 3 });    
 }
+
