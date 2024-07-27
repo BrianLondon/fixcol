8b4c5f88a952d8e1497d6b1812a87f7f1fc4161c
BrianLondon <brian@seatgeek.com>
2024-07-27 10:30:47 -0400

    Support for tuple structs added

--- fixed-derive/src/lib.rs
+++ fixed-derive/src/lib.rs
@@ -8,27 +8,79 @@ extern crate syn;
 use attrs::FieldConfig;
 use proc_macro::TokenStream;
 
-use quote::quote;
-use syn::{Data, DataStruct, DeriveInput, Fields};
+use quote::{format_ident, quote};
+use syn::{Data, DataStruct, DeriveInput, Fields, FieldsNamed, FieldsUnnamed};
 
 fn struct_read(fields: Fields) -> proc_macro2::TokenStream {
-    let fields = match fields {
-        Fields::Named(named_fields) => named_fields,
-        Fields::Unnamed(_) => todo!(),
-        Fields::Unit => todo!(),
-    };
+    match fields {
+        Fields::Named(named_fields) => struct_read_fields(named_fields),
+        Fields::Unnamed(unnamed_fields) => tuple_struct_read_fields(unnamed_fields),
+        Fields::Unit => panic!("Cannot deserialize type with no inner data"),
+    }
+}
 
+fn config_align(config: &FieldConfig) -> proc_macro2::TokenStream {
+    match &config.align {
+        attrs::Align::Left => quote! {fixed::Alignment::Left},
+        attrs::Align::Right => quote! {fixed::Alignment::Right},
+        attrs::Align::Full => quote! {fixed::Alignment::Full},
+    }
+}
+
+fn tuple_struct_read_fields(fields: syn::FieldsUnnamed) -> proc_macro2::TokenStream {
+    let field_reads = fields.unnamed.iter().enumerate().map(|item| {
+        let (field_num, field) = item;
+
+        let ident = format_ident!("f{}", field_num);
+
+        let config = attrs::parse_attributes(&field.attrs);
+        let FieldConfig { skip, width, align: _ } = config;
+
+        let alignment = config_align(&config);
+        let buf_size = skip + width;
+
+        // TODO: we shouldn't need a String here at all
+        let read = quote! {
+            let mut s: [u8; #buf_size] = [0; #buf_size];
+            buf.read_exact(&mut s).map_err(|e| fixed::error::Error::from(e))?;
+            let #ident = std::str::from_utf8(&s)
+                .map_err(|e| fixed::error::Error::from_utf8_error(&s, e))?
+                .parse_with(&fixed::FieldDescription {
+                    skip: #skip,
+                    len: #width,
+                    alignment: #alignment,
+                }).map_err(|e| fixed::error::Error::from(e))?;
+        };
+
+        (ident, read)
+    });
+
+    let (names, reads): (Vec<proc_macro2::Ident>, Vec<proc_macro2::TokenStream>) = field_reads.unzip();
+
+    let mut read_steps = proc_macro2::TokenStream::new();
+    read_steps.extend(reads.into_iter());
+
+    let mut name_steps = proc_macro2::TokenStream::new();
+    name_steps.extend(names.into_iter().map(|f| quote! { #f , }));
+
+    quote! {
+        fn read_fixed<R: std::io::Read>(buf: &mut R) -> Result<Self, fixed::error::Error> {
+            use fixed::FixedDeserializer;
+            #read_steps
+
+            Ok(Self(#name_steps))
+        }
+    }
+}
+
+fn struct_read_fields(fields: syn::FieldsNamed) -> proc_macro2::TokenStream {
     let field_reads = fields.named.iter().map(|field| {
         let name = field.ident.as_ref().unwrap().clone();
 
         let config = attrs::parse_attributes(&field.attrs);
-        let FieldConfig { skip, width, align } = config;
+        let FieldConfig { skip, width, align: _ } = config;
 
-        let alignment = match align {
-            attrs::Align::Left => quote! {fixed::Alignment::Left},
-            attrs::Align::Right => quote! {fixed::Alignment::Right},
-            attrs::Align::Full => quote! {fixed::Alignment::Full},
-        };
+        let alignment = config_align(&config);
 
         let buf_size = skip + width;
 
@@ -94,24 +146,13 @@ pub fn read_fixed_impl(input: TokenStream) -> TokenStream {
     gen.into()
 }
 
-// For now as a PoC we're just assuming ten characters per field
-fn struct_write(fields: Fields) -> proc_macro2::TokenStream {
-    let fields = match fields {
-        Fields::Named(named_fields) => named_fields,
-        Fields::Unnamed(_) => todo!(),
-        Fields::Unit => todo!(),
-    };
-
+fn write_named_fields(fields: FieldsNamed) -> proc_macro2::TokenStream {
     let field_writes = fields.named.iter().map(|field| {
         let name = field.ident.as_ref().unwrap().clone();
         let config = attrs::parse_attributes(&field.attrs);
-        let FieldConfig { skip, width, align } = config;
+        let FieldConfig { skip, width, align: _ } = config;
 
-        let alignment = match align {
-            attrs::Align::Left => quote! {fixed::Alignment::Left},
-            attrs::Align::Right => quote! {fixed::Alignment::Right},
-            attrs::Align::Full => quote! {fixed::Alignment::Full},
-        };
+        let alignment = config_align(&config);
 
         quote! {
             let _ = self.#name.write_fixed(
@@ -136,6 +177,50 @@ fn struct_write(fields: Fields) -> proc_macro2::TokenStream {
 
             Ok(())
         }
+    }
+}
+fn write_unnamed_fields(fields: FieldsUnnamed) -> proc_macro2::TokenStream {
+    let field_writes = fields.unnamed.iter().enumerate().map(|f| {
+        let (num, field) = f;
+        let name = syn::Index::from(num);
+        let config = attrs::parse_attributes(&field.attrs);
+        let FieldConfig { skip, width, align: _ } = config;
+
+        let alignment = config_align(&config);
+
+        quote! {
+            let _ = self.#name.write_fixed(
+                buf,
+                &fixed::FieldDescription {
+                    skip: #skip,
+                    len: #width,
+                    alignment: #alignment,
+                }
+            ).unwrap();
+        }
+    });
+
+    let mut write_steps = proc_macro2::TokenStream::new();
+    write_steps.extend(field_writes.into_iter());
+
+    quote! {
+        fn write_fixed<W: std::io::Write>(&self, buf: &mut W) -> Result<(), ()> {
+            use fixed::FixedSerializer;
+
+            #write_steps
+
+            Ok(())
+        }
+    }
+}
+
+// For now as a PoC we're just assuming ten characters per field
+fn struct_write(fields: Fields) -> proc_macro2::TokenStream {
+    match fields {
+        Fields::Named(named_fields) => write_named_fields(named_fields),
+        Fields::Unnamed(unnamed_fields) => write_unnamed_fields(unnamed_fields),
+        Fields::Unit => 
+            panic!("Unit structs not supported. Cannot serialize data type that hold no data"),
     }
 }
 
